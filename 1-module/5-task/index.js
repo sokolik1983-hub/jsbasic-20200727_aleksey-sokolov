dd9990e7a6db252ae177079f5b816b57bd3c186f
Aleksey Sokolov <lionsbet@outlook.com>
2020-08-03 23:31:51 +0300

    task5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,9 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength ) {
+    return str.slice(0 , maxlength-1 ) + "…";
+  } else {
+    return str;
+  }
 }
