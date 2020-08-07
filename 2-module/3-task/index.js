6d95f7de0cefa993ea93face6ff740fb843c55ff
Aleksey Sokolov <lionsbet@outlook.com>
2020-08-07 21:29:39 +0300

    zadaniye2 Task3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,15 @@
 let calculator = {
   // ваш код
 };
-
+calculator.read = function (c,d) {
+  calculator.stNum = c;
+  calculator.ndNum = d;
+  calculator.sum = function () {
+  return (calculator.stNum+calculator.ndNum);
+};
+    calculator.mul = function () {
+  return (calculator.stNum*calculator.ndNum);
+};
+}
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
