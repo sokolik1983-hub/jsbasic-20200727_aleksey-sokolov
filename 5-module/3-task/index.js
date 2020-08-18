82de4917a25d59ce92fabaa0b8a25721446deaa4
Aleksey Sokolov <lionsbet@outlook.com>
2020-08-18 21:50:18 +0300

    'module5__task3???'

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
+
+
+
+
+
+
 function initCarousel() {
-  // ваш код...
+  let rightBtn = document.querySelector('.carousel__arrow_right');
+let leftBtn = document.querySelector('.carousel__arrow_left');
+let contInner = document.querySelector('.carousel__inner');
+let contWidth = document.querySelector('.carousel__inner').offsetWidth;
+let contInnerStart = 0;
+  let allWidth = -(3*contWidth);
+  if(contInnerStart === 0) {
+    leftBtn.style.display = 'none';
+  };
+
+  leftBtn.addEventListener('click', () => {
+    contInner.style.translateX = contInnerStart + contWidth + "px";
+    contInnerStart += contWidth;
+    if(contInnerStart === 0) {
+      leftBtn.style.display = 'none';
+    };
+    if(contInnerStart <= 0) {
+      rightBtn.style.display = 'block';
+    }
+  });
+  rightBtn.addEventListener('click', () => {
+    contInner.style.translateX = contInnerStart - contWidth + "px";
+    contInnerStart -= contWidth;
+    if(contInnerStart < 0) {
+      leftBtn.style.display = 'block';
+    };
+    if(contInnerStart === allWidth) {
+      rightBtn.style.display = 'none';
+    };
+  });
+
+
+
+
+
 }
