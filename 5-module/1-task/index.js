28bddb5e9b7f87012ab1654b35bd8250d0939b76
Aleksey Sokolov <lionsbet@outlook.com>
2020-08-25 19:18:01 +0300

    '6module__task3'

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,4 +1,4 @@
 function hideSelf() {
   let button = document.querySelector('.hide-self-button');
-    button.addEventListener( "click" , () => button.setAttribute('hidden',''));
+  button.addEventListener("click", () => button.setAttribute('hidden', ''));
 }
